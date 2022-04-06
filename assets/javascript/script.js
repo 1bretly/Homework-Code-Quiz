9a1dcf3359e83e1bfeb740f69d45f841bfb7f138
Bret Andersen <1bretly@gmail.com>
2022-04-05 22:04:41 -0500

    updated js function/got question running

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,44 +1,4 @@
 // Variables
-let startButton = document.getElementById("start-btn");
-startButton.addEventListener("click", startQuiz);
-
-let nextButton = document.getElementById("next-btn");
-nextButton.addEventListener("click", nextQuestion);
-
-let timer = document.getElementById("timer");
-
-let questionContainer = document.getElementById("question-container");
-let shuffledQuestions, currentQuestionIndex;
-let questionEl = document.getElementById("question");
-let answerButtons = document.getElementById("answer-buttons");
-
-// Functions
-let totalTime = 150;
-
-function startQuiz() {
-    startButton.classList.add("hide")
-    questionContainer.classList.remove("hide");
-    shuffledQuestions = questions.sort(() => Math.random() -.5);
-    currentQuestionIndex = 0;
-    let startTimer = setInterval(function () {
-    totalTime--;
-    timer.textContent = totalTime;
-    if (totalTime === 0) {
-        clearInterval(startTimer);
-    }
-}, 1000)
-    nextQuestion();
-}
-
-function nextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-}
-
-
 // Questions in an array
 let questions = [
     {
@@ -59,54 +19,152 @@ let questions = [
             { text: "parenthesis", correct: false },
         ]
     },
-//     {
-//         question: "Arrays in JavaScript can be used to store _____.",
-//         choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
-//         answer: "b. other arrays"
-//     },
-//     {
-//         question: "Commonly used data types DO NOT include:",
-//         choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
-//         answer: "c. alerts"
-//     },
-//     {
-//         question: "How do you create a function in JavaScript",
-//         choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-//         answer: "b. function myFunction()"
-//     },
-//     {
-//         question: "How do you call a function named myFunction?",
-//         choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
-//         answer: "c. myFunctions()"
-//     },
-//     {
-//         question: "To see if two variables are equal in an if / else statement you would use ____.",
-//         choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
-//         answer: "b. =="
-//     },
-//     {
-//         question: "The first index of an array is ____.",
-//         choices: ["a. 0", "b. 1", "c. 8", "d. any"],
-//         answer: "a. 0"
-//     },
-//     {
-//         question: "Who invented JavaScript?",
-//         choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
-//         answer: "c. Brendan Eich"
-//     },
-//     {
-//         question: "How to write an IF statement in JavaScript?",
-//         choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
-//         answer: "c. if(i == 5)"
-//     },
-//     {
-//         question: "How do you add a comment in a JavaScript?",
-//         choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
-//         answer: "a. //This is a comment"
-//     },
-//     {
-//         question: "Which event occurs when the user clicks on an HTML element?",
-//         choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-//         answer: "a. onclick"
-//     }
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        answers: [
+            { text: "other arrays", correct: true },
+            { text: "booleans", correct: false },
+            { text: "numbers", correct: false },
+            { text: "functions", correct: false },
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "alerts", correct: true },
+            { text: "numbers", correct: false },
+        ]
+    },
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        answers: [
+            { text: "=", correct: false },
+            { text: "!=", correct: false },
+            { text: "<=>", correct: false },
+            { text: "==", correct: true },
+        ]
+    },
+    {
+        question: "The first index of an array is ____.",
+        answers: [
+            { text: "0", correct: true },
+            { text: "1", correct: false },
+            { text: "2", correct: false },
+            { text: "3", correct: false },
+        ]
+    },
+    {
+        question: "Who invented JavaScript?",
+        answers: [
+            { text: "Dick Hertz", correct: false },
+            { text: "Brendan Eich", correct: true },
+            { text: "John Javascript", correct: false },
+            { text: "Bill Gates", correct: false },
+        ]
+    },
 ];
+
+
+// Buttons for start and answers with addEventListener
+let startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startQuiz);
+
+
+// Question & answer
+let questionContainer = document.getElementById("question-container");
+let shuffledQuestions, currentQuestionIndex;
+let questionEl = document.getElementById("question");
+let answerButtons = document.getElementById("answer-buttons");
+
+
+// Timer
+let timer = document.getElementById("timer");
+let totalTime = 150;
+let score = 0;
+
+
+// Functions
+function startQuiz() {
+    startButton.classList.add("hide")
+    questionContainer.classList.remove("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    let startTimer = setInterval(function () {
+        totalTime--;
+        timer.textContent = totalTime;
+        if (totalTime === 0) {
+            clearInterval(startTimer);
+        }
+    }, 1000)
+    nextQuestion();
+}
+
+function nextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach((answer, index) => {
+        let button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerButtons.appendChild(button);
+    })
+}
+
+function resetState() {
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+        selectAnswer();
+    }
+}
+
+function selectAnswer(e) {
+    document.querySelectorAll(".btn").forEach((item) => {
+        item.addEventListener("click", (event) => {
+            if (
+                event.currentTarget.innerText === questions[currentQuestionIndex].answers
+            ) {
+                score++;
+            } else {
+                timer -= 15;
+            }
+            if (
+                timer == 0
+            ) 
+            {
+            endQuiz();
+        } else {
+            nextQuestion();
+        }
+        currentQuestionIndex++;
+    if (currentQuestionIndex > 6) {
+        endQuiz();
+    } else {
+        nextQuestion();
+    }
+});
+    });
+}
+
+function endQuiz() {
+    let nameInput = document.createElement("input");
+    let scoreSubmit = document.createElement("button");
+    quizContainer.innerHTML = "";
+    nameInput.setAttribute("id", "nameInput");
+    scoreSubmit.setAttribute("id", "scoreSubmit");
+    scoreSubmit.innerText = "Submit";
+    quizContainer.append(nameInput);
+    quizContainer.append(scoreSubmit);
+    scoreSubmit.addEventListener("click", submitScore);
+    document.getElementById("startButton").remove();
+    startTime = 0;
+  } 
