97136c5a7d9afe180f1893ce8b481e5715259247
Bret Andersen <1bretly@gmail.com>
2022-04-25 10:01:32 -0500

    fixed score and timer bug

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,123 @@
+// Start button with addEventListener
+let startButton = document.getElementById("start-btn")
+startButton.addEventListener("click", startQuiz)
+
+// More variables
+let shuffledQuestions, currentQuestionIndex
+let questionContainer = document.getElementById("question-container")
+let questionEl = document.getElementById("question")
+let answerButtons = document.getElementById("answer-buttons")
+let instructionsEl = document.getElementById("instructions")
+let timer = document.getElementById("timer")
+let totalTime = 60
+let score = 0
+
+// Functions
+function startQuiz() {
+  startButton.classList.add("hide")
+  instructionsEl.classList.add("hide")
+  timer.classList.remove("hide")
+  questionContainer.classList.remove("hide")
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0
+  let startTimer = setInterval(function () {
+    totalTime--
+    timer.textContent = totalTime
+    if (totalTime === 0) {
+      clearInterval(startTimer)
+    }
+  }, 1000)
+  nextQuestion()
+}
+
+function nextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question
+  question.answers.forEach((answer, index) => {
+    let button = document.createElement("button")
+    button.innerText = answer.text
+    button.classList.add("btn")
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer)
+    answerButtons.appendChild(button)
+  })
+}
+
+function resetState() {
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild)
+    selectAnswer()
+  }
+}
+
+function selectAnswer(e) {
+  document.querySelectorAll(".btn").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      console.log(event.target)
+      console.log(event.target.dataset.correct === true)
+      if (event.target.dataset.correct === "true") {
+        score++
+        console.log(score)
+      } else {
+        totalTime -= 15
+      }
+      if (totalTime == 0) {
+        endQuiz()
+      } else {
+        nextQuestion()
+      }
+      currentQuestionIndex++
+      if (currentQuestionIndex > 6) {
+        endQuiz()
+      } else {
+        nextQuestion()
+      }
+    })
+  })
+}
+
+function endQuiz() {
+  let nameInput = document.createElement("input")
+  let scoreSubmit = document.createElement("button")
+  questionContainer.innerHTML = ""
+  nameInput.setAttribute("id", "nameInput")
+  scoreSubmit.setAttribute("id", "scoreSubmit")
+  scoreSubmit.innerText = "Submit"
+  questionContainer.append(nameInput)
+  questionContainer.append(scoreSubmit)
+  scoreSubmit.addEventListener("click", submitScore)
+  document.getElementById("start-btn").remove()
+  timer = 0
+}
+
+function submitScore() {
+  let scores = []
+  let name = document.getElementById("nameInput").value
+  if (localStorage.getItem("scores")) {
+    scores = JSON.parse(localStorage.getItem("scores"))
+  }
+  scores.push({
+    name: name,
+    score: score,
+  })
+  localStorage.setItem("scores", JSON.stringify(scores))
+  displayScores(scores)
+}
+function displayScores(scores) {
+  questionContainer.innerHTML = ""
+  for (entry of scores) {
+    let scoreEntry = document.createElement("p")
+    scoreEntry.innerText = `Name: ${entry.name}, Score: ${entry.score}`
+    questionContainer.append(scoreEntry)
+  }
+}
+
 // Questions in an array
 let questions = [
   {
@@ -66,123 +186,3 @@ let questions = [
     ],
   },
 ]
-
-// Start button with addEventListener
-let startButton = document.getElementById("start-btn")
-startButton.addEventListener("click", startQuiz)
-
-// More variables
-let shuffledQuestions, currentQuestionIndex
-let questionContainer = document.getElementById("question-container")
-let questionEl = document.getElementById("question")
-let answerButtons = document.getElementById("answer-buttons")
-let instructionsEl = document.getElementById("instructions")
-let timer = document.getElementById("timer")
-let totalTime = 150
-let score = 0
-
-// Functions
-function startQuiz() {
-  startButton.classList.add("hide")
-  instructionsEl.classList.add("hide")
-  timer.classList.remove("hide")
-  questionContainer.classList.remove("hide")
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
-  currentQuestionIndex = 0
-  let startTimer = setInterval(function () {
-    totalTime--
-    timer.textContent = totalTime
-    if (totalTime === 0) {
-      clearInterval(startTimer)
-    }
-  }, 1000)
-  nextQuestion()
-}
-
-function nextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionEl.innerText = question.question
-  question.answers.forEach((answer, index) => {
-    let button = document.createElement("button")
-    button.innerText = answer.text
-    button.classList.add("btn")
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener("click", selectAnswer)
-    answerButtons.appendChild(button)
-  })
-}
-
-function resetState() {
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild)
-    selectAnswer()
-  }
-}
-
-function selectAnswer(e) {
-  document.querySelectorAll(".btn").forEach((item) => {
-    item.addEventListener("click", (event) => {
-      if (
-        event.currentTarget.innerText ===
-        questions[currentQuestionIndex].answers
-      ) {
-        score++
-      } else {
-        totalTime -= 15
-      }
-      if (totalTime == 0) {
-        endQuiz()
-      } else {
-        nextQuestion()
-      }
-      currentQuestionIndex++
-      if (currentQuestionIndex > 6) {
-        endQuiz()
-      } else {
-        nextQuestion()
-      }
-    })
-  })
-}
-
-function endQuiz() {
-  let nameInput = document.createElement("input")
-  let scoreSubmit = document.createElement("button")
-  questionContainer.innerHTML = ""
-  nameInput.setAttribute("id", "nameInput")
-  scoreSubmit.setAttribute("id", "scoreSubmit")
-  scoreSubmit.innerText = "Submit"
-  questionContainer.append(nameInput)
-  questionContainer.append(scoreSubmit)
-  scoreSubmit.addEventListener("click", submitScore)
-  document.getElementById("start-btn").remove()
-  timer = 0
-}
-
-function submitScore() {
-  let scores = []
-  let name = document.getElementById("nameInput").value
-  if (localStorage.getItem("scores")) {
-    scores = JSON.parse(localStorage.getItem("scores"))
-  }
-  scores.push({
-    name: name,
-    score: score,
-  })
-  localStorage.setItem("scores", JSON.stringify(scores))
-  displayScores(scores)
-}
-function displayScores(scores) {
-  questionContainer.innerHTML = ""
-  for (entry of scores) {
-    let scoreEntry = document.createElement("p")
-    scoreEntry.innerText = `Name: ${entry.name}, Score: ${entry.score}`
-    questionContainer.append(scoreEntry)
-  }
-}
